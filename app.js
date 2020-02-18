095e6a25a39eac2493a8cbdbe59f1ea38e643c69
Shriram <shriram@kinshipsystems.com>
2020-02-18 15:24:58 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+var apos = require('apostrophe')({
+  shortName: 'test-lug',
+
+  // See lib/modules for basic project-level configuration of our modules
+  // responsible for serving static assets, managing page templates and
+  // configuring user accounts.
+
+  modules: {
+
+    // Apostrophe module configuration
+
+    // Note: most configuration occurs in the respective
+    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+    
+    // However any modules that are not present by default in Apostrophe must at
+    // least have a minimal configuration here: `moduleName: {}`
+
+    // If a template is not found somewhere else, serve it from the top-level
+    // `views/` folder of the project
+
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+
+  },
+  
+  // This configures our default page template
+  'apostrophe-pages': {
+    types: [
+      //This is the new section
+      {
+        name: 'sensnewtemp',
+        label: 'Sendhuraan New Template'
+      },
+      //The rest is what what was there already
+      {
+        name: 'home',
+        label: 'Home'
+      },
+    ]
+  }
+});
